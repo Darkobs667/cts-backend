740070ebf2e2bbe73b42f35b390e2d35d12400af
sow221 <sowmjr21@gmail.com>
2026-05-14 22:42:50 +0000

    ------------

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,6 @@ use App\Http\Controllers\Api\CandidateController;
 use App\Http\Controllers\Api\VoteController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\PhysicalVoteController;
-use App\Http\Controllers\Api\InviteCodeController;
 
 // =============================================
 // ROUTES PUBLIQUES
@@ -72,8 +71,4 @@ Route::middleware('auth:api')->group(function () {
 
     Route::post('/positions/{positionId}/physical-votes', [PhysicalVoteController::class, 'store'])->middleware('admin');
 
-    // Codes d'invitation
-    Route::get('/invite-codes',              [InviteCodeController::class, 'index'])->middleware('admin');
-    Route::post('/invite-codes/generate',    [InviteCodeController::class, 'generate'])->middleware('admin');
-    Route::delete('/invite-codes/{id}',      [InviteCodeController::class, 'destroy'])->middleware('admin');
 });
